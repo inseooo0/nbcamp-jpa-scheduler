62feff9d2d77e0b46ff31457838a9b7d682ad93f
inseooo0 <calmandpassion@korea.ac.kr>
2024-08-28 14:39:24 +0900

    test: comment test - update 검증 메서드 추가

--- src/test/java/nbcamp/jpascheduler/service/CommentServiceTest.java
+++ src/test/java/nbcamp/jpascheduler/service/CommentServiceTest.java
@@ -8,6 +8,7 @@ import nbcamp.jpascheduler.domain.Comment;
 import nbcamp.jpascheduler.domain.Schedule;
 import nbcamp.jpascheduler.domain.User;
 import nbcamp.jpascheduler.dto.CommentCreateDto;
+import nbcamp.jpascheduler.dto.CommentUpdateDto;
 import nbcamp.jpascheduler.dto.ScheduleCreateDto;
 import nbcamp.jpascheduler.dto.UserCreateDto;
 import org.assertj.core.api.Assertions;
@@ -30,8 +31,8 @@ class CommentServiceTest {
     @Transactional
     public void save() {
 
-        User user = saveUser();
-        Schedule schedule = saveSchedule();
+        User user = saveUser("UserA", "UserA@email.com");
+        Schedule schedule = saveSchedule("schedule title", "schedule content");
 
         CommentCreateDto commentCreateDto = new CommentCreateDto();
         commentCreateDto.setContent("test comment content");
@@ -44,21 +45,52 @@ class CommentServiceTest {
         Assertions.assertThat(saved).isEqualTo(findComment);
     }
 
+    @Test
     @Transactional
-    public User saveUser() {
+    public void update() {
+
+        User userA = saveUser("UserA", "UserA@email.com");
+        Schedule schedule = saveSchedule("schedule title", "schedule content");
+
+        CommentCreateDto commentCreateDto = new CommentCreateDto();
+        commentCreateDto.setContent("test comment content");
+        commentCreateDto.setUserId(userA.getId());
+        commentCreateDto.setScheduleId(schedule.getId());
+
+        Comment saved = commentService.save(commentCreateDto);
+
+        User userB = saveUser("UserB", "UserB@email.com");
+        Schedule newSchedule = saveSchedule("new schedule", "content");
+
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
+        commentUpdateDto.setContent("update Content");
+        commentUpdateDto.setUserId(userB.getId());
+        commentUpdateDto.setScheduleId(newSchedule.getId());
+
+        Comment updated = commentService.update(saved.getId(), commentUpdateDto);
+        Comment findComment = commentService.findById(updated.getId());
+
+        Assertions.assertThat(updated).isEqualTo(findComment);
+        Assertions.assertThat(updated.getUser()).isEqualTo(userB);
+        Assertions.assertThat(updated.getSchedule()).isEqualTo(newSchedule);
+        Assertions.assertThat(updated.getContent()).isEqualTo("update Content");
+    }
+
+    @Transactional
+    public User saveUser(String username, String email) {
         UserCreateDto userCreateDto = new UserCreateDto();
-        userCreateDto.setName("UserA");
-        userCreateDto.setEmail("userA@email.com");
+        userCreateDto.setName(username);
+        userCreateDto.setEmail(email);
         userCreateDto.setPassword("0000");
         return userService.save(userCreateDto);
     }
 
     @Transactional
-    public Schedule saveSchedule() {
+    public Schedule saveSchedule(String title, String content) {
         ScheduleCreateDto scheduleCreateDto = new ScheduleCreateDto();
         scheduleCreateDto.setUserIds(List.of(1L));
-        scheduleCreateDto.setContent("schedule content");
-        scheduleCreateDto.setTitle("test schedule");
+        scheduleCreateDto.setContent(content);
+        scheduleCreateDto.setTitle(title);
         return scheduleService.save(scheduleCreateDto);
     }
 }
